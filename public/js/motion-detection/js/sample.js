b222fb33099e32fb7f565f780629dd77124ad0e5
Aruna Herath <aruna@kadira.io>
2015-11-07 14:20:23 +0530

    Detects motion 'left to right' or 'right to left'

--- public/js/motion-detection/js/sample.js
+++ public/js/motion-detection/js/sample.js
@@ -1,7 +1,6 @@
 (function(){
 	// consider using a debounce utility if you get too many consecutive events
 	$(window).on('motion', function(ev, data){
-		console.log('detected motion at', new Date(), 'with data:', data);
 		var spot = $(data.spot.el);
 		spot.addClass('active');
 		setTimeout(function(){
@@ -11,10 +10,42 @@
 
 	// examples for id usage
 	$('#top-left').on('motion', function(){
-		console.log('left');
+		var now = Date.now()
+		if((Date.now()-touched['left'])>500){
+			touched['left'] = now
+			calcShot('left')
+		};
 	});
 
 	$('#top-right').on('motion', function(){
-		console.log('right');
+		var now = Date.now()
+		if((Date.now()-touched['right'])>500){
+			touched['right'] = now
+			calcShot('right')
+		};
 	});
 })();
+
+var touched = {
+	'right': 0,
+	'left': 0
+}
+var shots = {}
+
+function calcShot(me){
+	var now = Date.now();
+
+	var lastTouchedTime = 0;
+	var lastTouched;
+	for(var spot in touched){
+		if(spot===me) continue;
+		if(touched[spot]>lastTouchedTime){
+			lastTouchedTime=touched[spot]
+			lastTouched = spot
+		}
+	}
+
+	if(now - lastTouchedTime < 500){
+		console.log('shot ' + lastTouched + ' to ' + me);
+	}
+}
